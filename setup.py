2a6c55547f88d0ce29ef6ea1cfc0f2722aea8143
aokuyama <ao980s@gmail.com>
2022-02-13 14:29:36 +0900

    pip packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+import setuptools
+ 
+setuptools.setup(
+    name="playwright_super_driver",
+    version="0.0.1",
+    author="aokuyama",
+    author_email="mail@aokuyama.work",
+    description="python playwright sample package",
+    long_description="python playwright sample package",
+    long_description_content_type="text/markdown",
+    url="https://web.aokuyama.work",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
